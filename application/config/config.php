a2841fd4614ee4ae389a2fe11611b5ce21e91d8c
sorapan <rudeboy04forplay@gmail.com>
2016-04-19 11:57:26 +0700

    fix merge

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://te.st/tidti/';
+$config['base_url'] = 'http://aaa/tidti';
 
 /*
 |--------------------------------------------------------------------------
