49922b2324f1e6fcf889a28a0aac90431adbbaae
sorapan <rudeboy04forplay@gmail.com>
2016-04-23 14:54:24 +0700

    update ui

--- application/views/login.php
+++ application/views/login.php
@@ -7,23 +7,34 @@
 <div class="contain col-xs-12 nopad">
     <div class="login  col-md-offset-2 col-xs-12 col-sm-12 col-md-8 nopad">
         <div class="_1 col-xs-12 col-sm-4">
+                <h2 class="opensans bold">SrivijayaWifi <i class="fa fa-wifi wifi" aria-hidden="true"></i></h2>
 
+                <h3 class="opensans bold">e-passport</h3>
+                <p class=" thaisans">ลงทะเบียนอุปกรณ์เพื่อเข้าใช้งานอินเตอร์เน็ต</p>
+                <div class="footer">
+                    <h2 class="thaisans">ติดต่อ</h2>
+                    <p><i class="fa fa-phone" aria-hidden="true"></i> : 074-317100 ต่อ 1911 - 1912</p>
+                    <p><i class="fa fa-envelope-o" aria-hidden="true"></i> : noc@rmutsv.ac.th</p>
+                </div>
         </div>
         <div class="_2 col-xs-12 col-sm-8">
 
-            <div class="form  col-xs-12 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10">
+            <!-- <div class="form  col-xs-12 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10">
                 <div class="head">
                     <h2 class="thaisans bold">เข้าสู่ระบบ</h2>
                 </div>
-                
-                
-                <form method="post" action="std_logincheck">
-                    
+ -->
+
+                <form method="post" class="form  col-xs-12 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10" action="std_logincheck">
+                <!-- <div class="form  col-xs-12 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10"> -->
+                    <div class="head">
+                        <h2 class="thaisans bold">เข้าสู่ระบบ</h2>
+                    </div>
                     <div class="input">
                         <div class="inputbox">
                             <div class="col-xs-offset-1 col-xs-10 col-sm-10 col-md-offset-1 col-md-10 nopad">
                                 <label><i class="glyphicon glyphicon-user"></i></label>
-                                <input type="text" class="id opensans" name="e_pass" id="" placeholder="e-passport" value="s155404130050">
+                                <input type="text" class="id opensans" name="e_pass" id="" placeholder="e-passport" value="">
                             </div>
                         </div>
                         <div class="inputbox">
@@ -50,9 +61,13 @@
                         <div class="submit">
                             <input type="submit" class="" value="เข้าสู่ระบบ">
                         </div>
-                    </div>
-                    
+
+                    <!-- </div> -->
+
                 </form>
+
+
+
 
         </div>
 
--- application/views/student/index.php
+++ application/views/student/index.php
@@ -7,14 +7,20 @@
     <div class="login user-page col-md-offset-2 col-xs-12 col-sm-12 col-md-8 nopad">
         <div class="_1 col-xs-12 col-sm-4">
             <div class="head">
-                <h2 class="thaisans bold">นักศึกษา</h2>
+                <h2 class="thaisans">นักศึกษา</h2>
             </div>
-            <div class="content">
-                <a href="student/signout">ออกจากระบบ</a>
+            <div class="content thaisans">
+
                 <div class="name"><?=prefix_name_id($this->session->userdata('prefix_name_id'))?> <?= $this->session->userdata('firstname')?> <?= $this->session->userdata('lastname')?></div>
-                <div class="major">รหัส <?= $this->session->userdata('id')?></div>
-                <div class="major">คณะ <?=fac_id($this->session->userdata('fac_id'))?></div>
-                <div class="major">สาขา <?=program_id($this->session->userdata('program_id'))?></div>
+                <div class="epassport">รหัส <?= $this->session->userdata('id')?></div>
+                <!-- <div class="faculty">คณะ <?=fac_id($this->session->userdata('fac_id'))?></div>
+                <div class="major">สาขา <?=program_id($this->session->userdata('program_id'))?></div> -->
+                <a href="student/signout" class="signout"><i class="fa fa-sign-out" title="ออกจากระบบ" aria-hidden="true"></i>&nbspออกจากระบบ</a>
+            </div>
+            <div class="footer">
+                <h2 class="thaisans">ติดต่อ</h2>
+                <p><i class="fa fa-phone" aria-hidden="true"></i> : 074-317100 ต่อ 1911 - 1912 </p>
+                <p> <i class="fa fa-envelope-o" aria-hidden="true"></i> : noc@rmutsv.ac.th</p>
             </div>
         </div>
         <div class="_2 col-xs-12 col-sm-8">
@@ -26,16 +32,23 @@
                 <div class="sub-head">
                     <h2 class="thaisans">ลงทะเบียน mac-address เพื่อรับสัญญาณ Internet</h2>
                 </div>
+
+                <!-- //////////////////////////////////////////////////////////// -->
+                <!-- ////////////            alert                  ////////////// -->
+                <div class="alert">
+                    <div class="user_alert">
+                        <h2 class="thaisans"></h2>
+                    </div>
+                </div>
                 <div class="content col-xs-10">
                         <div class="add-device">
                             <!--<div class="alert">alert</div>-->
-                            <div class="device">
 
 
                             <!--/////////////////////////////////////////////////////////////////////////-->
-                            
+
                                 <h3 class="thaisans bold">คอมพิวเตอร์/โน็ตบุ๊ค</h3>
-                                
+
                             <?php
                                 $data_exists = false;
                                 foreach($mac_data as $data){
@@ -47,20 +60,20 @@
                                 }
                                 if($data_exists){
                             ?>
-                                
+
                                 <form method="POST" action="student/deletemac" onsubmit="return confirm('Are you sure you want to submit this form?');">
                                     <div class="ch-device activated">
                                         <input type="text" class="text opensans" disabled name="" value="<?=$data->mac?>" id="">
                                         <button class="button"><i class="fa fa-trash-o"></i></button>
-                                        <label for="laptop"><i class="fa fa-laptop active"></i></label>
+                                        <label for="laptop" class="laptop"><i class="fa fa-laptop active"></i></label>
                                     </div>
                                     <input type="hidden" name="del" value="<?=$data->mac?>">
                                 </form>
-                                
+
                             <?php
                                 }else{
                             ?>
-                            
+
                                 <form method="POST" action="student/addmac">
                                   <div class="ch-device ">
                                       <input type="text" class="text opensans" placeholder="mac-address" name="mac" id="">
@@ -69,16 +82,16 @@
                                   </div>
                                   <input type="hidden" name="device" value="comp">
                                 </form>
-                                
+
                             <?php
                                 }
-                            
+
                             ?>
-                            
+
                             <!--/////////////////////////////////////////////////////////////////////////-->
-                                
+
                                 <h3 class="thaisans bold">โทรศัพท์</h3>
-                                
+
                             <?php
                             $data_exists = false;
                             foreach($mac_data as $data){
@@ -90,35 +103,35 @@
                             }
                                 if($data_exists){
                             ?>
-                                
+
                                 <form method="POST" action="student/deletemac" onsubmit="return confirm('Are you sure you want to submit this form?');">
                                     <div class="ch-device activated">
                                         <input type="text" class="text opensans" disabled name="" value="<?=$data->mac?>" id="">
                                         <button class="button"><i class="fa fa-trash-o"></i></button>
-                                        <label for="mobile"><i class="fa fa-mobile active"></i></label>
+                                        <label for="mobile" class="mobile"><i class="fa fa-mobile active"></i></label>
                                     </div>
                                     <input type="hidden" name="del" value="<?=$data->mac?>">
                                 </form>
-                                
+
                             <?php
-                                
+
                                 }else{
                             ?>
-                            
+
                                 <form method="POST" action="student/addmac">
                                   <div class="ch-device ">
                                       <input type="text" class="text opensans" name="mac" placeholder="mac-address" id="">
                                       <button class="button" type="submit"><i class="fa fa-plus-square-o"></i></button>
-                                      <label for="mobile"><i class="fa fa-mobile"></i></label>
+                                      <label for="mobile" class="mobile"><i class="fa fa-mobile"></i></label>
                                   </div>
                                   <input type="hidden" name="device" value="phone">
                                 </form>
-                                
+
                             <?php
                                 }
-                            
+
                             ?>
-                                
+
                                 <!--<form method="POST">
                                   <div class="ch-device ">
                                       <input type="text" class="text opensans" name="mac_mobile" placeholder="mac-address" id="">
@@ -126,11 +139,11 @@
                                       <label for="mobile"><i class="fa fa-mobile"></i></label>
                                   </div>
                                 </form>-->
-                                
+
                             <!--/////////////////////////////////////////////////////////////////////////-->
-                            
+
                                 <h3 class="thaisans bold">แท็ปเล็ต</h3>
-                                
+
                             <?php
                                     $data_exists = false;
                             foreach($mac_data as $data){
@@ -143,21 +156,21 @@
                             }
                                 if($data_exists){
                             ?>
-                            
-                                                       
+
+
                                 <form method="POST" action="student/deletemac" onsubmit="return confirm('Are you sure you want to submit this form?');">
                                     <div class="ch-device activated">
                                         <input type="text" class="text opensans" disabled name="" value="<?=$data->mac?>" id="">
                                         <button class="button"><i class="fa fa-trash-o"></i></button>
-                                        <label for="tablet"><i class="fa fa-tablet active"></i></label>
+                                        <label for="tablet" class="tablet"><i class="fa fa-tablet active"></i></label>
                                     </div>
                                     <input type="hidden" name="del" value="<?=$data->mac?>">
                                 </form>
-                                
+
                             <?php
                                 }else{
                             ?>
-                                
+
                                 <form method="POST" action="student/addmac">
                                   <div class="ch-device ">
                                       <input type="text" class="text opensans" name="mac" placeholder="mac-address" id="">
@@ -169,22 +182,13 @@
                             <?php
                                 }
                             ?>
-
-                                <!--<div class="ch-device activated">
-                                    <input type="text" class="text opensans" disabled name="" placeholder="mac-address" id="">
-                                    <button class="button"><i class="fa fa-trash-o"></i></button>
-                                    <label for="mobile"><i class="fa fa-mobile active"></i></label>
-                                </div>-->
-                                
-
-                            </div>
                         </div>
-                    </div>
-            </div>
 
+                    </div>
 
 
         </div>
+
 
     </div>
 </div>
