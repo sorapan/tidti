c8fa2cc83442dd661f9e01a91dfdb227125bb739
sorapan <rudeboy04forplay@gmail.com>
2016-06-20 18:54:28 +0700

    sss

--- application/config/config.php
+++ application/config/config.php
@@ -23,14 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-<<<<<<< HEAD
 
-
-
-=======
 // $config['base_url'] = 'http://aaa/tidti';
     $config['base_url'] = 'http://localhost/www/tidti';
->>>>>>> parent of e90f27e... professor_page fix
+
 
 /*
 |--------------------------------------------------------------------------
