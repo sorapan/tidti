694c42058ccf19df1081147912ab8b69e76c202a
sorapan <rudeboy04forplay@gmail.com>
2016-06-20 19:06:45 +0700

    sssssssqaswqww

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
+// $config['base_url'] = 'http://aaa/tidti';
+    $config['base_url'] = 'http://localhost/www/tidti';
 
 /*
 |--------------------------------------------------------------------------
