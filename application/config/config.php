cea157d083e7623a556b5e3b546c5ea4fea52d42
bestzaba <bestzaba@users.noreply.github.com>
2016-06-22 15:40:30 +0700

    test commit from other where [2]

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://aaa/tidti';
-    $config['base_url'] = 'http://localhost/www/tidti';
+    $config['base_url'] = 'http://localhost/tidti';
 
 /*
 |--------------------------------------------------------------------------
