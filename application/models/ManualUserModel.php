edeb6a5f7b6ee813807227849f01308a2028cf41
bestzaba <blf_forever@hotmail.co.th>
2016-07-01 18:58:31 +0700

    edit manual_user
    to manualuser

--- application/models/ManualUserModel.php
+++ application/models/ManualUserModel.php
@@ -18,27 +18,27 @@ class ManualUserModel extends CI_Model {
     public function GetDataManualUserByWhere($where){
         $this->db->db_select('radius');
         $this->db->select('*');
-        // $this->db->from('manual_user');
+        // $this->db->from('manualuser');
         $this->db->where('firstname',$where['firstname']);
         $this->db->where('lastname',$where['lastname']);
         $this->db->where('idcard',$where['idcard']);
-        // $this->db->get('manual_user')->result();
+        // $this->db->get('manualuser')->result();
         // $this->db->where($where);
         // return $this->output->enable_profiler(TRUE);
-        return $this->db->get('manual_user')->result();
+        return $this->db->get('manualuser')->result();
     }
 
     public function AddDataManualUser($data){
         $this->db->db_select('radius');
-        $this->db->insert('manual_user',$data);
+        $this->db->insert('manualuser',$data);
         // $this->session->set_flashdata('alert', 'เพิ่มข้อมูลสำเร็จ');
-        @header('Location:'.base_url().'/admin');
+        // @header('Location:'.base_url().'/admin');
         // return $this->output->enable_profiler(TRUE);
     }
 
     function DeleteManual($username){
         $this->db->db_select('radius');
-        $this->db->delete('manual_user',array(
+        $this->db->delete('manualuser',array(
                 'username'=>$username
             ));
     }
